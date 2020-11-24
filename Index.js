aeed6f2a0f99e42dbfab17505b904390d608a9ae
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2020-11-23 20:49:42 -0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,13 @@
+Discord = require('discord.js');
+DB = require('quick.db');
+Bot = new Discord.Client();
+Prefix = ("Enter Prefix Here"); // Use Your Own Prefix Idea //
+Token = ("Enter Token Here"); // Use Your Own Bot Token //
+Status = ("Online")
 
+Bot.once('ready', () => {
+  console.info(`Bot: ${Bot.user.tag}`);
+  console.info(`Bot Status: ${Status}!`);
+});
+
+Bot.login(Token)
